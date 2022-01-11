13d698c459d7cc9524f29b9701b7e9b2f79a87e7
Shane Gadsby <schme16@gmail.com>
2022-01-11 10:25:28 +1000

    Updating syntax styles

--- main.js
+++ main.js
@@ -1,64 +1,62 @@
-module.exports = (function respond(cmd, ops){
+module.exports = ((cmd, ops) => {
 
-	var t;
-
-	var spawn = require('child_process').spawn(cmd, ops)
-	var stdout = spawn.stdout.on('data', function(d){
-		var data = d.toString().trim();
-		for(var i in t.arrays.debug){
-			t.arrays.debug[i](data);
-		}
-
-		if(t.arrays.on[data]){
-			spawn.stdin.write(t.arrays.on[data]+'');
-			delete t.arrays.on[data]
-			var k = 0;
-			for (var i in t.arrays.on) {
-				k++;
+	let t,
+		spawn = require('child_process').spawn(cmd, ops),
+		stdout = spawn.stdout.on('data', (d) => {
+			let data = d.toString().trim();
+			for (let i in t.arrays.debug) {
+				t.arrays.debug[i](data);
 			}
 
-			if (k == 0) {
-				spawn.stdin.end();
+			if (t.arrays.on[data]) {
+				spawn.stdin.write(t.arrays.on[data] + '');
+				delete t.arrays.on[data]
+				let k = 0;
+				for (let i in t.arrays.on) {
+					k++;
+				}
+
+				if (k === 0) {
+					spawn.stdin.end();
+				}
 			}
-		}
 
 
-
-	}).on('error', function(data){
-		for(var i in t.arrays.error){
-			t.arrays.error[i](data);
-		}
-	}).on('close', function(data){
-		for(var i in t.arrays.end){
-			t.arrays.end[i](data);
-		}
-	});
+		}).on('error', (data) => {
+			for (let i in t.arrays.error) {
+				t.arrays.error[i](data);
+			}
+		}).on('close', (data) => {
+			for (let i in t.arrays.end) {
+				t.arrays.end[i](data);
+			}
+		});
 
 	t = {
 
 		arrays: {
-			end:[],
-			on:{},
-			debug:[],
-			error:[],
+			end: [],
+			on: {},
+			debug: [],
+			error: [],
 		},
 
-		error: function(func){
+		error: (func) => {
 			this.arrays.error.push(func);
 			return this;
 		},
 
-		on: function(a,b){
+		on: (a, b) => {
 			this.arrays.on[a] = b;
 			return this;
 		},
 
-		end: function(func){
+		end: (func) => {
 			this.arrays.end.push(func);
 			return this;
 		},
 
-		debug: function(func){
+		debug: (func) => {
 			this.arrays.debug.push(func);
 			return this
 		},
